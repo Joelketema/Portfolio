ab50c7b835d9322fc9cc8de1672bab5a4f3bc85f
Eyuel Ketema <eyuelket2019@gmail.com>
2022-04-15 16:36:29 -0700

    fixed email

--- src/Components/Email.js
+++ src/Components/Email.js
@@ -1,19 +1,36 @@
-
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 import '../Style/emailStyle.css'
 import '../Style/projectStyle.css'
 const Email = () => {
-    return (
+
+    const formRef = useRef(null);
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_66lix3m', 'template_9bsv5ak', formRef.current, 'BnKeqyEEXFs7AK4o9')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        formRef.current.reset();
+    };
+
+  
+    return (  
     
-        <form action="submit">
+        <form action="submit"  ref={formRef} onSubmit={sendEmail}>
         <div className="email">
                 <h2>Email Me for <span className="name"> any comments or Jobs!</span></h2>
                 <div className="userinputs">
-                    <input type="text" placeholder="Your Name" name="" id="NameSend" required/>
-                    <input type="email" name="" placeholder="Your Email" id="EmailSend" required/>
+                    <input type="text" placeholder="Your Name" name="name" id="NameSend" required/>
+                    <input type="email" name="email" placeholder="Your Email" id="EmailSend" required/>
                 </div>
-                <textarea name="" id="" cols="50" placeholder="Place Your Message Here" rows="10" required></textarea>
+                <textarea id="" name="message" cols="50" placeholder="Place Your Message Here" rows="10" required></textarea>
                 <input type="submit" value="Send Email" id="myText" />
-        </div>
+        </div> 
         </form>
 
     )
